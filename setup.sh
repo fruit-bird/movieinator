df2cd76858e85b8a68b0c1fa438fca8d4e556bd9
fruit-bird <github.ru3w9@slmail.me>
2023-04-08 20:55:02 +0000

    setup script tweak

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 MOVIE_DATABASE_URL='sqlite:///Users/<username>/Documents/movies.db'
 
 # Add the export command to the ~/.zshrc file
-echo "# Movienator database path (<repo link here>)" >> ~/.zshrc
+echo "# Movienator database path (https://github.com/fruit-bird/movieinator)" >> ~/.zshrc
 echo "export MOVIE_DATABASE_URL=\"$MOVIE_DATABASE_URL\"" >> ~/.zshrc
 
 # Reload the ~/.zshrc file
